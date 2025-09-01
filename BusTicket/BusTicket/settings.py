6ecb343b7e508496c130b3b39c047a4716a861c6
Yinwailwin <ppink3356@gmail.com>
2025-09-01 13:41:36 +0630

    error message for contact us

--- BusTicket/BusTicket/settings.py
+++ BusTicket/BusTicket/settings.py
@@ -176,3 +176,13 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+    messages.INFO: 'info',
+    messages.WARNING: 'warning',
+    messages.DEBUG: 'secondary',
+}
